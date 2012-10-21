6201e0311d3d20dc5ba16dd3693af3c6cc0deaf5
qurandev <qurandev>
2012-10-21 20:17:15 +0530

    minor

--- js/app.js
+++ js/app.js
@@ -8,9 +8,27 @@ var app = angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.dir
     $routeProvider.when('/A2', {templateUrl: 'content/A/A2.html', controller: synonymsController});
     $routeProvider.when('/A3', {templateUrl: 'content/A/A3.html', controller: synonymsController});
     $routeProvider.when('/A4', {templateUrl: 'content/A/A4.html', controller: synonymsController});
+	$routeProvider.when('/A5', {templateUrl: 'content/A/A5.html', controller: synonymsController});
+	
+	$routeProvider.when('/A6', {templateUrl: 'content/A/A6.html', controller: synonymsController});
+	$routeProvider.when('/A7', {templateUrl: 'content/A/A7.html', controller: synonymsController});
+	$routeProvider.when('/A8', {templateUrl: 'content/A/A8.html', controller: synonymsController});
+	$routeProvider.when('/A9', {templateUrl: 'content/A/A9.html', controller: synonymsController});
+	$routeProvider.when('/A10', {templateUrl: 'content/A/A10.html', controller: synonymsController});
+	
+	$routeProvider.when('/:primaryNav/:secondaryNav', {
+            templateUrl: 'content/A/A2.html', //'resources/angular/templates/nav/urlRouter.html',
+            controller: 'RouteController'
+        });
+		
     $routeProvider.otherwise({redirectTo: '/A1'});
   }]);
   
+  
+  function RouteController($scope, $routeParams) {
+        //$scope.templateUrl = 'resources/angular/templates/nav/'+$routeParams.primaryNav+'/'+$routeParams.secondaryNav+'.html';
+		$scope.templateUrl = 'content/A/'+$routeParams.primaryNav+ /*'/'+$routeParams.secondaryNav */ '.html';
+    }
   
   
   app.filter('startFrom', function() {
