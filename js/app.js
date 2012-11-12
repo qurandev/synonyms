f9bdafa7dbf97d8db3502da56e4110f7ea334116
qurandev <qurandev>
2012-11-12 16:37:14 +0530

    Building out searhable index page (not yet enabled)

--- js/app.js
+++ js/app.js
@@ -5,30 +5,48 @@
 var app = angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
   config(['$routeProvider', function($routeProvider) {
     //$routeProvider.when('/A1', {templateUrl: 'content/A/A1.html', controller: synonymsController});
-    //$routeProvider.when('/A2', {templateUrl: 'content/A/A2.html', controller: synonymsController});
-    //$routeProvider.when('/A3', {templateUrl: 'content/A/A3.html', controller: synonymsController});
 	$routeProvider.when('', {
 			templateUrl: 'content/urlrouter.html',
 			controller: 'synonymsController'
 	});
-	$routeProvider.when('/:primaryNav', {// '/:secondaryNav', {
-            templateUrl: 'content/urlrouter.html', //'resources/angular/templates/nav/urlRouter.html',
-            controller: 'RouteController'
+	$routeProvider.when('/index', {
+			templateUrl: 'partials/index.html',
+			controller: 'indexController'
+	});
+	$routeProvider.when('/topics', {
+			templateUrl: 'partials/topics.html',
+			controller: 'topicsController'
+	});
+	$routeProvider.when('/roots', {
+			templateUrl: 'partials/roots.html',
+			controller: 'rootsController'
+	});
+//	$routeProvider.when('/:primaryNav', {// '/:secondaryNav', {
+//            templateUrl: 'content/urlrouter.html',
+//            controller: 'RouteController'
+//    });
+	$routeProvider.when('/:primaryNav/:mode', {// '/:secondaryNav', {
+            templateUrl: 'content/urlrouter.html',
+            controller: 'synonymsController'
     });
-		
-    $routeProvider.otherwise({redirectTo: '/A1'});
+    //$routeProvider.otherwise({redirectTo: '/A1'});
   }]);
   
   
   function RouteController($scope, $routeParams) {
-		var path = '', number='', hash = $routeParams.primaryNav;
+		var path = '', number='', hash = $routeParams.primaryNav, mode = $routeParams.mode;
 		if(hash){
 			var arr = hash.match(/\d+$/), arr2 = hash.match(/[^\d]+/);
 			if(arr && arr[0]){	number = arr[0]; }
 			if(arr2 && arr2[0]){ path = arr2[0]; } console.log(path +' '+ number);
 		}
         //$scope.templateUrl = 'resources/angular/templates/nav/'+$routeParams.primaryNav+'/'+$routeParams.secondaryNav+'.html';
-		$scope.templateUrl = 'content/' + path + '/'+$routeParams.primaryNav+ '.html'; // /*'/'+$routeParams.secondaryNav */ '.html';
+		$scope.templateUrl = 'content/' + path + '/'+$routeParams.primaryNav+ '.html';
+		$scope.mode = mode;
+		var IDs = ['', 'trans', 'book', 'pdf', 'info'];
+		if( _.indexOf(IDs, mode) != -1){
+			$scope.tab = _.indexOf(IDs, mode);
+		}
 		setTimeout("urlCheck('" + $scope.templateUrl + "', '" + hash + "')", 0);
     }
 	
