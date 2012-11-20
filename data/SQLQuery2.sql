9f5fa1fe56900941ecf3f151216cfbd8f54a71c3
qurandev <qurandev>
2012-11-20 09:14:21 +0530

    data misc

---
+++ data/SQLQuery2.sql
@@ -0,0 +1,19 @@
+USE [Quran]
+GO
+
+/****** Object:  Table [dbo].[Synonyms]    Script Date: 11/18/2012 23:57:09 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[SynTest](
+	[id] [nchar](10) NOT NULL,
+	[topic] [nvarchar](255) NOT NULL,
+	[topicUr] [nvarchar](255) NOT NULL,
+) ON [PRIMARY]
+
+GO
+
+
