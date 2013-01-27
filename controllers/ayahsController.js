83a347a6fb41c2de8b723f6e9850ee72db030aae
Q D <qurandev@gmail.com>
2013-01-27 11:16:53 +0530

    added quran app page

--- controllers/ayahsController.js
+++ controllers/ayahsController.js
@@ -1,9 +1,10 @@
-var fetchSura, format, findTopicsForSura, findSurasForTopic, topicsAyahsMap, getSynonym;
+var fetchSura, format, findTopicsForSura, findSurasForTopic, topicsAyahsMap, getSynonym, suraNames;
 
 var ayahsController = function($scope, $route, $routeParams, $location, $http, $rootScope){console.log('ayahsController ' + JSON.stringify($routeParams));
 	$scope.qurandata = "dsffsdfs";
 	$scope.ayahhtml = "ayah htmmmmmmmmmmmml";
 	$rootScope.topicsAyahsMap = topicsAyahsMap;
+	$rootScope.suraNames = suraNames;
 	$rootScope.findTopicsForSura = findTopicsForSura;
 	$rootScope.findSurasForTopic = findSurasForTopic;
 //	$rootScope._ = _;
@@ -11,7 +12,7 @@ var ayahsController = function($scope, $route, $routeParams, $location, $http, $
 	
 	$rootScope.suwar = [];
 	_.each( _.range(1, 115), function(i, surano){
-		$rootScope.suwar.push( { id: ++surano, group: parseInt( 1 + surano/10 ), label: 'Surah ' + surano });
+		$rootScope.suwar.push( { id: ++surano, group: parseInt( 1 + surano/10 ), label: surano + (suraNames ? ': '+ suraNames[surano] : '') });
 	});
 	
 	$rootScope.fetchSura = function(ref){
@@ -228,7 +229,4 @@ topicsAyahsMap = [
 
 ]
 
-//var sura = 36, id = "A6";
-//fetchSura( sura );
-//findTopicsForSura( sura );
-//findSurasForTopic( id );
+suraNames = ["","Al-Faatiha","Al-Baqara","Aal-i-Imraan","An-Nisaa","Al-Maaida","Al-An'aam","Al-A'raaf","Al-Anfaal","At-Tawba","Yunus","Hud","Yusuf","Ar-Ra'd","Ibrahim","Al-Hijr","An-Nahl","Al-Israa","Al-Kahf","Maryam","Taa-Haa","Al-Anbiyaa","Al-Hajj","Al-Muminoon","An-Noor","Al-Furqaan","Ash-Shu'araa","An-Naml","Al-Qasas","Al-Ankaboot","Ar-Room","Luqman","As-Sajda","Al-Ahzaab","Saba","Faatir","Yaseen","As-Saaffaat","Saad","Az-Zumar","Al-Ghaafir","Fussilat","Ash-Shura","Az-Zukhruf","Ad-Dukhaan","Al-Jaathiya","Al-Ahqaf","Muhammad","Al-Fath","Al-Hujuraat","Qaaf","Adh-Dhaariyat","At-Tur","An-Najm","Al-Qamar","Ar-Rahmaan","Al-Waaqia","Al-Hadid","Al-Mujaadila","Al-Hashr","Al-Mumtahana","As-Saff","Al-Jumu'a","Al-Munaafiqoon","At-Taghaabun","At-Talaaq","At-Tahrim","Al-Mulk","Al-Qalam","Al-Haaqqa","Al-Ma'aarij","Nooh","Al-Jinn","Al-Muzzammil","Al-Muddaththir","Al-Qiyaama","Al-Insaan","Al-Mursalaat","An-Naba","An-Naazi'aat","Abasa","At-Takwir","Al-Infitaar","Al-Mutaffifin","Al-Inshiqaaq","Al-Burooj","At-Taariq","Al-A'laa","Al-Ghaashiya","Al-Fajr","Al-Balad","Ash-Shams","Al-Lail","Ad-Dhuhaa","Ash-Sharh","At-Tin","Al-Alaq","Al-Qadr","Al-Bayyina","Az-Zalzala","Al-Aadiyaat","Al-Qaari'a","At-Takaathur","Al-Asr","Al-Humaza","Al-Fil","Quraish","Al-Maa'un","Al-Kawthar","Al-Kaafiroon","An-Nasr","Al-Masad","Al-Ikhlaas","Al-Falaq","An-Naas"]
